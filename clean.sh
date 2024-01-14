ebcbeb3bf3b0c61bf0796c1c4126c33c09786570
Chris <chrisriley3d@gmail.com>
2024-01-14 15:57:57 -0600

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd ~/
 rm -Rf ~/3DC_Build
-git clone -b Testing https://github.com/brotherchris/3DC_Build.git
+git clone https://github.com/brotherchris/3DC_Build.git
 cd 3DC_Build
 chmod 777 3DC_NP_Build.sh
 chmod 777 clean.sh
